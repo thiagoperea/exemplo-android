01d0d92944eb9f2fb9be6c7cf15a82e2622c0866
Thiago <thiago.perea1@gmail.com>
2020-08-17 15:13:14 -0300

    preloaded font

---
+++ app/src/main/java/com/github/thiagoperea/testeeasynvest/presentation/SimulationStartActivity.kt
@@ -0,0 +1,13 @@
+package com.github.thiagoperea.testeeasynvest.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.thiagoperea.testeeasynvest.R
+
+class SimulationStartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simulation_start)
+    }
+}
